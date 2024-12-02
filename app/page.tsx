eb1f2c14eb08ddc97ec0e56b2fbaf3a0ad80178c
Ezecodes-Phantom <osakwechibueze@gmail.com>
2024-12-02 22:07:50 +0100

    commits

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 
+import Companies from "@/components/Companies";
 import Hero from "@/components/Hero";
 
 export default function Home() {
 	return (
 		<div>
 			<Hero />
+			<Companies />
 		</div>
 	);
 }
