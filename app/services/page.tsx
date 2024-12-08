cf9cc950ac6430566b13773ab1c96f9a6219bda7
Ezecodes-Phantom <osakwechibueze@gmail.com>
2024-12-08 03:58:02 +0100

    building

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -22,13 +22,13 @@ const ServicesPage = () => {
 					</span>
 				</h1>
 			</div>
-			<div className="flex flex-wrap w-full py-10 px-5 gap-10 justify-center">
+			<div className="flex flex-wrap w-full py-10 px-5 gap-y-10  justify-center md:gap-x-10">
 				{services.map((service, index) => (
 					<div key={index} className="flex flex-col ">
 						<div className="max-w-lg">
 							<Carousel autoSlide={true} autoSlideInterval={5000}>
 								{service.images.map((img, index) => (
-									<Image key={index} src={img} alt={service.name} width={600} height={600} className="rounded" />
+									<Image key={index} src={img} alt={service.name} width={600} height={600} />
 								))}
 							</Carousel>
 						</div>
@@ -47,6 +47,16 @@ const ServicesPage = () => {
 				<h1 className="font-semibold text-3xl leading-[50px] md:text-4xl md:leading-[60px] text-gray-300">
 					- Serving all of Southern California and Beyond -
 				</h1>
+			</div>
+
+			<div className="flex justify-center items-center text-center py-10">
+				<h2 className="font-bold">
+					Recent Projects
+					<hr className="h-1 border-b-solid border-[2px] border-[#e8bc37] outline-none w-36 my-3 rounded-full" />
+				</h2>
+				<div>
+					<Link href=""></Link>
+				</div>
 			</div>
 		</section>
 	);
